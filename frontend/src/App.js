afd7b5b315242827bea12fa4a4ae73e471796d73
MichelleL <168042638+michelle-loi@users.noreply.github.com>
2023-11-16 01:25:49 -0700

    commit directory

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/login";
+import Registration from "./pages/registration"
+
+function App() {
+  return (
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login/>}/> {/* This routes initially to the login page. For path / means first page write the others as you need*/}
+            <Route path="/registration" element={<Registration/>}/>
+
+
+
+          </Routes>
+        </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/registration.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const registration = () => {
+    return(
+        <div>Register</div>
+    )
+}
+
+export default registration
