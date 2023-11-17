088cdaaff16108f94675b1f7c10299718d5c347e
Ryan <ryan.loi@ucalgary.ca>
2023-11-16 18:37:29 -0700

    created an initial page not found redirect - will improve upon this later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Login from "./pages/login";
 import Registration from "./pages/registration"
+import PageNotFound from "./pages/pageNotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login/>}/> {/* This routes initially to the login page. For path / means first page write the others as you need*/}
             <Route path="/registration" element={<Registration/>}/>
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ frontend/src/pages/pageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pageNotFound.css"
+
+const PageNotFound = () => {
+  return(
+      <div className="Error page">
+          <h1>Error 404: Page Not Found</h1>
+      </div>
+  )
+}
+
+export default PageNotFound
