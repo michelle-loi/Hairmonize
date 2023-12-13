e661da90853b0b5642d19e06b1b8f069d6137dec
anna-jeongah <annajlee28@gmail.com>
2023-12-12 18:24:58 -0700

    Added citations

--- frontend/src/components/adminHome/viewClients.jsx
+++ frontend/src/components/adminHome/viewClients.jsx
@@ -1,3 +1,10 @@
+/**
+ * The following code in this file is written with assistance from these YouTube videos:
+ * Lama Dev. (2022, September 18). React Node.js MySQL CRUD Tutorial for Beginners [Video]. YouTube. https://www.youtube.com/watch?v=fPuLnzSjPLE
+ * Lama Dev. (2022, September 26). React Node.js MySQL Full Stack Blog App Tutorial [Video]. YouTube. https://www.youtube.com/watch?v=0aPLk2e2Z3g&list=PLiNtaYczsCu3L-F8BrVsGNIt_7iQDxsLF&index=10
+ * Code With Yousaf. (2023, March 28). React + Node js + MySQL - CRUD Operations | CRUD Rest API with Node and Express [Video]. YouTube. https://www.youtube.com/watch?v=y5NvOade3sk&list=PLiNtaYczsCu3L-F8BrVsGNIt_7iQDxsLF&index=14&t=1125s
+ */
+
 //import React from "react";
 import React, { useState, useEffect } from 'react';
 import { Container, Form, Button, Row, Col, Table } from 'react-bootstrap';
--- frontend/src/components/stylistHome/BookAppointments.jsx
+++ frontend/src/components/stylistHome/BookAppointments.jsx
@@ -1,3 +1,10 @@
+/**
+ * The following code in this file is written with assistance from these YouTube videos:
+ * Lama Dev. (2022, September 18). React Node.js MySQL CRUD Tutorial for Beginners [Video]. YouTube. https://www.youtube.com/watch?v=fPuLnzSjPLE
+ * Lama Dev. (2022, September 26). React Node.js MySQL Full Stack Blog App Tutorial [Video]. YouTube. https://www.youtube.com/watch?v=0aPLk2e2Z3g&list=PLiNtaYczsCu3L-F8BrVsGNIt_7iQDxsLF&index=10
+ * Code With Yousaf. (2023, March 28). React + Node js + MySQL - CRUD Operations | CRUD Rest API with Node and Express [Video]. YouTube. https://www.youtube.com/watch?v=y5NvOade3sk&list=PLiNtaYczsCu3L-F8BrVsGNIt_7iQDxsLF&index=14&t=1125s
+ */
+
 import React, {useState, useContext, useEffect} from "react";
 import {Container, Dropdown, DropdownButton, Row, Col, Form, Button} from "react-bootstrap";
 import {AuthContext} from "../../context/authContext";
